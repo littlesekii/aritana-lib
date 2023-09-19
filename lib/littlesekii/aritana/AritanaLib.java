8066f1a8ea65e358c258a01f51246a7a72f1708b
littlesekii <littlesekii@gmail.com>
2023-09-19 20:23:47 -0300

    Project started

---
+++ lib/littlesekii/aritana/AritanaLib.java
@@ -0,0 +1,9 @@
+package lib.littlesekii.aritana;
+
+public class AritanaLib {
+    final static private String LIB_VERSION = "v0.0.1";
+
+    public static String version() {
+        return "Aritana Library " + LIB_VERSION;
+    }
+}
